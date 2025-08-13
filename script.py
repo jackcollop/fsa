041bee467684da42514ad1138fcc7c2792b46b6d
jackcollop <jackcollop@gmail.com>
2025-08-13 10:37:56 -0500

    Update script.py

--- script.py
+++ script.py
@@ -38,17 +38,9 @@ driver = webdriver.Chrome(service=service, options=options)
 driver.execute_cdp_cmd(
     "Page.setDownloadBehavior", {"behavior": "allow", "downloadPath": os.getcwd()}
     )
-driver.get('https://apps.fsa.usda.gov/sorspub/reports/web/public/loan-maturity-national')
 
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
-
-select = Select(driver.find_element(by=By.ID, value='commodity'))
-select.select_by_visible_text('COTTON-UP')
-
-time.sleep(3)
-
-driver.execute_script("javascript:submitRequest('/sorspub/reports','csv')")
 
 time.sleep(1)
 
